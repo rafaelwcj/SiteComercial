7fe1640f051ab1788748c46b189b41cf826dd71a
Rafael <rafael_cerqueira1@hotmail.com>
2021-01-16 17:34:33 -0300

    adicionando servidor node

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static(path.join(__dirname, 'dist/Site')));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'dist/Site/index.html'));
+});
+
+const port = process.env.PORT || 3000;
+app.set('port', port);
+const server = http.createServer(app);
+app.listen(port, () => {
+    console.log('Servidor iniciado na porta ' + port);
+});
